dcf797ac934fc481899be38874042de3918df9f1
Alexandros <alexandros.kazalis@hotmail.com>
2025-06-07 22:33:25 +0200

    created a middleware to log every response that does not have status code 200.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
-const port = 8080;
+const PORT = 8080;
 
-app.listen(port);
+app.use('/app', express.static('./src/app'));
+app.use(middlewareLogResponses);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.get('/healthz', handlerReadiness);
+
+function handlerReadiness(req: Request, res: Response) {
+  res.status(200).set('Content-Type', 'text/plain; charset=utf-8').send('OK');
+}
+
+function middlewareLogResponses(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  res.on('finish', () => {
+    if (res.statusCode !== 200) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`
+      );
+    }
+  });
+  next();
+}
